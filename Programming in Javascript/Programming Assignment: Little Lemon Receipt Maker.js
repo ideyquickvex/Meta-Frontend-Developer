14ce76328ec0af5c45366a3d57776cfc02979f3b
ideyquickvex <109346384+ideyquickvex@users.noreply.github.com>
2023-12-28 11:33:14 +0100

    Create Programming Assignment: Little Lemon Receipt Maker.js

---
+++ Programming in Javascript/Programming Assignment: Little Lemon Receipt Maker.js
@@ -0,0 +1,67 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices(taxBoolean) {
+    for (key of Object.keys(dishData)) {
+        let finalPrice ;
+        let name = dishData[key].name
+        let price = dishData[key].price
+        if (taxBoolean == true) {
+            finalPrice = price * tax;
+        }
+        else if (taxBoolean == false) {
+            finalPrice = price
+        }
+        else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return;
+        }
+        console.log(`Dish: ${name} Price: $${finalPrice}`)
+    }
+}
+
+// Implement getDiscount()
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+    try {
+        if (typeof guests === "number" && guests > 0 && guests < 30) {
+            var discount = 0
+            if (guests < 5) {
+                discount = 5;
+            }
+            else if (guests >= 5) {
+                discount = 10;
+            }
+            console.log('Discount is: $' + discount)
+        }
+        else {
+            console.log('The second argument must be a number between 0 to 30')
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+// Call getDiscount()
+getDiscount(true, 2)
+getDiscount(false, 10)
